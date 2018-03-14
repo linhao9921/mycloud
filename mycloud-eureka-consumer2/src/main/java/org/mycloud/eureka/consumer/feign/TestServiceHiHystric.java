fe99e7ab22ae7c4944d02a820d7c871184f610c7
linhao <hao.lin@w-oasist.com>
2018-03-14 10:53:11 +0800

    增加Hystrix处理类

---
+++ mycloud-eureka-consumer2/src/main/java/org/mycloud/eureka/consumer/feign/TestServiceHiHystric.java
@@ -0,0 +1,22 @@
+package org.mycloud.eureka.consumer.feign;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 林浩<hao.lin@w-oasis.com>
+ * @version 创建时间：2018年3月14日 上午10:15:43
+ * 说明：断路器
+ */
+@Component
+public class TestServiceHiHystric implements TestService{
+
+    /**
+     * 当http://mycloud-eureka-producer/hi?name=无法访问时，熔断执行此方法处理
+     * @param name
+     * @return
+     */
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "hi," + name + ",sorry,error!";
+    }
+}
